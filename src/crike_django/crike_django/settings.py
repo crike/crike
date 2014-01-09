f6034abf911526fd3a805a28f977257411cbb6b3
GeekAn <geekan@foxmail.com>
2014-01-09 21:19:42 +0800

    fix sqlite location

--- src/crike_django/crike_django/settings.py
+++ src/crike_django/crike_django/settings.py
@@ -20,7 +20,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'E:\\GitHub\\crike\\src\\crike_django\\crike_django\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': PROJECT_DIR + '/crike_django/sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
