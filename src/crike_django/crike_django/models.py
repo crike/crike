57f030ccd2df7e12cddb6ee036b7bf6a511c1373
geekan <geekan@foxmail.com>
2014-01-12 02:24:04 -0800

    fix codingstyle to pep8, left some error

--- src/crike_django/crike_django/models.py
+++ src/crike_django/crike_django/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from djangotoolbox.fields import EmbeddedModelField, ListField
 
-# 数据库基本模型分为word、lesson(embedded)、book、user、course、voice、image、game、video
-# 当前目标：实现word、book、user、course，其余皆往后排
-# book包含多个lesson，lesson包含多个word，user包含多个course
+'''
+数据库基本模型分为word、lesson(embedded)、book、user、course、voice、image、game、video
+当前目标：实现word、book、user、course，其余皆往后排
+book包含多个lesson，lesson包含多个word，user包含多个course
+'''
+
 
 class Word(models.Model):
     name = models.CharField(max_length=50)
@@ -13,22 +16,28 @@ class Word(models.Model):
     mean = ListField(models.CharField(max_length=80))
     #image = ImageField()
 
+
 class Lesson(models.Model):
     name = models.CharField(max_length=50)
     words = ListField(models.ForeignKey('Word'))
+
 
 class Book(models.Model):
     name = models.CharField(max_length=50)
     lessons = ListField(EmbeddedModelField('Lesson'))
 
+
 class CET4Book(Book):
     pass
+
 
 class CET6Book(Book):
     pass
 
+
 class WebsterBook(Book):
     pass
+
 
 # This class is to keep compability with other apps
 # which use original User model.
@@ -62,19 +71,23 @@ class Profile(models.Model):
     class Meta:
         abstract = True
 
+
 # These fields use multi-table inheritance. See below urls for more details.
 #   https://docs.djangoproject.com/en/1.5/topics/db/models/#multi-table-inheritance
 #   http://stackoverflow.com/questions/3100521/django-registration-and-multiple-profiles
 # Another related work for more performance:
 #   http://onlypython.group.iteye.com/group/wiki/1519-expansion-django-user-model-by-non-profile-way
 class Teacher(Profile):
-    pass
+    ori_school = models.CharField(max_length=140)
+
 
 class Student(Profile):
     edu_stage = models.CharField(max_length=140)
 
+
 class TeachingAssistant(Profile):
     pass
+
 
 class Course(models.Model):
     pass
