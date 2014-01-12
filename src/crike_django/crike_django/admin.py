39eebaae31db0736f76fd15c6fbaad2c7affbe5e
geekan <geekan@foxmail.com>
2014-01-12 07:03:51 -0800

    Implement TeacherInline to make it a admin

--- src/crike_django/crike_django/admin.py
+++ src/crike_django/crike_django/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
 from models import Teacher, Student, Book, Lesson, Word
+
+
+class TeacherInline(admin.StackedInline):
+    model = Teacher
+    can_delete = False
+    verbose_name_plural = 'teacher'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (TeacherInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Teacher)
 admin.site.register(Student)
