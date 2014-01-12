e266349d3fb10a86aca11876421a7f2074cd4a5b
geekan <geekan@foxmail.com>
2014-01-12 02:08:50 -0800

    register models to admin

--- src/crike_django/crike_django/admin.py
+++ src/crike_django/crike_django/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from models import Teacher, Student
+from models import Teacher, Student, Book, Lesson, Word
 
 admin.site.register(Teacher)
 admin.site.register(Student)
+admin.site.register(Book)
+admin.site.register(Lesson)
+admin.site.register(Word)
