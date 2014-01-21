9ea09ef78f5fdf482449bd2938389dcec19b9809
geekben <bn0418@gmail.com>
2014-01-21 23:51:57 +0800

    Fix bug of word show feedback

--- src/crike_django/crike_django/views.py
+++ src/crike_django/crike_django/views.py
@@ -213,19 +213,18 @@ class LessonShowView(TemplateView):
         paginator = Paginator(words_list, 1)
         page = request.GET.get('page')
         words = get_words_from_paginator(paginator, page)
+        print "nnnnnnnnn"
+        print "word %s show!" % words[0]
+        print "nnnnnnnnn"
 
         lesson_result = LessonStat.objects.get_or_create(user=request.user,
                                                            lesson=lesson_obj)[0]
         pick = lesson_result.pick
         print 'Show with LessonStat: ', lesson_result
-        if page:
-            print "nnnnnnnnn"
-            print "word %s done!" % words_list[eval(page)-2]
-            print "nnnnnnnnn"
-            if eval(page) == len(words_list):
-                return render(request, self.template_name,
-                        {'words': words, 'book': book, 'lesson': lesson, 'done':'True',
-                        'lesson_result': lesson_result})
+        if page and eval(page) == len(words_list):
+            return render(request, self.template_name,
+                    {'words': words, 'book': book, 'lesson': lesson, 'done':'True',
+                    'lesson_result': lesson_result})
 
         return render(request, self.template_name,
                {'words': words, 'book': book, 'lesson': lesson,
@@ -267,9 +266,6 @@ class LessonPickView(TemplateView):
 
     def get(self, request, book, lesson):
         words_list = get_words_from_lesson(book, lesson)
-        print "nnnnnnnnn"
-        print "word %s done!" % words_list[len(words_list)-1]
-        print "nnnnnnnnn"
         paginator = Paginator(words_list, 1)
         page = request.GET.get('page')
         words = get_words_from_paginator(paginator, page)
