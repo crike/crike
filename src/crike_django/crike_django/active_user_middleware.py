de596fb6067f7062b940ef169b58506bd2a57175
GeekAn <geekan@foxmail.com>
2014-02-13 00:02:06 +0800

    middleware: give online user 5 points every 30 minutes

--- src/crike_django/crike_django/active_user_middleware.py
+++ src/crike_django/crike_django/active_user_middleware.py
@@ -1,12 +1,34 @@
 import datetime
 from django.core.cache import cache
 from django.conf import settings
+from crike_django.views import get_profile
 
 class ActiveUserMiddleware:
+    '''
+    cache all user visit time.
+    when an user is online for 30 minutes, give 5 points
+    '''
+    hour = 60 * 60
 
     def process_request(self, request):
+        if request.user.is_authenticated() is False:
+            return
+
         current_user = request.user
-        if request.user.is_authenticated():
-            now = datetime.datetime.now()
-            cache.set('seen_%s' % (current_user.username), now, 
-                           settings.USER_LASTSEEN_TIMEOUT)
+        now = datetime.datetime.now()
+        if cache.get('seen_%s' % (current_user.username)):
+            start_seen = cache.get('start_seen_%s' % (current_user.username))
+            if start_seen:
+                delta = now - start_seen
+                if delta > datetime.timedelta(minutes=30):
+                    p = get_profile(current_user)
+                    p.point_add(5)
+                    p.save()
+                    # Refresh start_seen time after points given.
+                    cache.set('start_seen_%s' % (current_user.username), now,
+                              self.hour)
+            else:
+                cache.set('start_seen_%s' % (current_user.username), now,
+                          self.hour)
+        cache.set('seen_%s' % (current_user.username), now,
+                  settings.USER_LASTSEEN_TIMEOUT)
