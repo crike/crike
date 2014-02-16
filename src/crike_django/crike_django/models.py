9dc8cf46a257e70baf96329351600a5f4963a106
geekben <bn0418@gmail.com>
2014-02-16 12:25:06 +0800

    model: lesson's main key is ID, every student has a strange words lesson

--- src/crike_django/crike_django/models.py
+++ src/crike_django/crike_django/models.py
@@ -60,7 +60,7 @@ class Word(models.Model):
 
 
 class Lesson(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
     book = models.ForeignKey('Book')
     words = ListField(models.ForeignKey('Word'))
     tag = models.CharField(max_length=50, blank=True, default='new')
