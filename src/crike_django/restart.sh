7ef3196a7eff0959d41907507bdfc1b49af52209
geekben <bn0418@gmail.com>
2014-02-17 20:35:47 +0800

    misc: Add IP and port to restart.sh

--- src/crike_django/restart.sh
+++ src/crike_django/restart.sh
@@ -1,5 +1,5 @@
 # NOTE: This script will kill all django processes.
-pkill -f 'python manage.py runserver'
+pkill -f 'python manage.py runserver 0.0.0.0:8080'
 export PYTHONIOENCODING=utf-8
 python manage.py runserver 0.0.0.0:8080
 ps aux | grep --color \[m\]anage\.py
