89691eb7c4940fd1585d7c0229e7433375cef84b
geekben <bn0418@gmail.com>
2014-04-03 20:14:40 +0800

    fix a typo

--- src/crike_django/backupdb.sh
+++ src/crike_django/backupdb.sh
@@ -3,4 +3,4 @@
 # 30 18   * * 7   root    sh /media/sf_GitHub/crike/src/crike_django/backupdb.sh
 
 DATE=`date +"%Y-%m-%d"`
-mongodump --db vinedb --out /opt/backup/mongodb-dump-$DATE
+mongodump --db crikedb --out /opt/backup/mongodb-dump-$DATE
