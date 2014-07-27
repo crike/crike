37c961a62ca71aebb0b9804ea946005c460cbbf8
geekben <bn0418@gmail.com>
2014-07-27 17:16:58 +0800

    Add comment to note restore method

--- src/crike_django/backupdb.sh
+++ src/crike_django/backupdb.sh
@@ -2,5 +2,8 @@
 # add a line to "/etc/crontab" like:
 # 30 18   * * 7   root    sh /media/sf_GitHub/crike/src/crike_django/backupdb.sh
 
+# for restore:
+# mongorestore [--drop] /opt/backup/mongodb-dump-$DATE/crikedb
+
 DATE=`date +"%Y-%m-%d"`
 mongodump --db crikedb --out /opt/backup/mongodb-dump-$DATE
